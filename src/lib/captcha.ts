02481f3dea73fbf74e44621f0d998dc457040932
Edgar Tolete <edgartolete@outlook.com>
2026-01-04 12:01:37 +0800

    fixing

--- src/lib/captcha.ts
+++ src/lib/captcha.ts
@@ -1,15 +1,28 @@
-export async function reCaptchaVerify(secretkey: string, token: string) {
+type ReCaptchaResT = {
+  success: boolean;
+  score?: number; // v3 only
+  action?: string; // v3 only
+  challenge_ts?: string;
+  hostname?: string;
+  "error-codes"?: string[];
+};
+
+export async function reCaptchaVerify(
+  secretKey: string,
+  token: string,
+): Promise<ReCaptchaResT> {
   const URL = `https://www.google.com/recaptcha/api/siteverify`;
+
+  const params = new URLSearchParams();
+  params.append("secret", secretKey);
+  params.append("response", token);
 
   const res = await fetch(URL, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: JSON.stringify({
-      secret: secretkey,
-      response: token,
-    }),
+    body: params.toString(),
   });
   return await res.json();
 }
